4a745b39265e23c39b312b8430fd63d18908d786
tama-7010 <otama.engine@gmail.com>
2019-03-04 23:50:21 +0900

    add Tokens

--- Go/InterpreterInGo/monkey/token/token.go
+++ Go/InterpreterInGo/monkey/token/token.go
@@ -18,8 +18,15 @@ const (
 	INT   = "INT"
 
 	// 演算子
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// デリミタ
 	COMMA     = ","
