e32c2362dda3a15077846944887ca6713613ff17
tama-7010 <otama.engine@gmail.com>
2019-03-12 00:17:06 +0900

    add file

--- C/meikai_alg/bubble.c
+++ C/meikai_alg/bubble.c
@@ -8,13 +8,14 @@
 void bubble(int a[], int n)
 {
     int i, j;
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = n - 1; j > i; j--) {
-            if (a[j - 1] > a[j]) {
-                swap(int, a[j - 1], a[j]);
-            }
+
+    for (i = 0; i < n - 1; i++) {
+        int exchange = 0;
+        for (j = n; j > i; j--) {
+            if (a[j - 1] > a[j]) swap(int, a[j - 1], a[j]);
+            exchange++;
         }
+        if (exchange == 0) break;
     }
 }
 
