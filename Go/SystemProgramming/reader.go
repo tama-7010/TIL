5bef4e47c92995e8710d109fcd6a78a351ee4fd2
tama-7010 <otama.engine@gmail.com>
2019-03-27 23:22:21 +0900

    add file

---
+++ Go/SystemProgramming/reader.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+var source = `
+一行目
+二行目
+三行目`
+
+func main() {
+	scanner := bufio.NewScanner(strings.NewReader(source))
+	for scanner.Scan() {
+		fmt.Printf("%#v\n", scanner.Text())
+	}
+}
