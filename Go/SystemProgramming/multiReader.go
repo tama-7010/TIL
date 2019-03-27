7b0ccfbdcc50bd4bd14e0fc1a639e4f9acfbc214
tama-7010 <otama.engine@gmail.com>
2019-03-28 00:13:00 +0900

    add file

---
+++ Go/SystemProgramming/multiReader.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+func main() {
+	header := bytes.NewBufferString("----- HEADER -----\n")
+	content := bytes.NewBufferString("Example of io.MultiReader\n")
+	footer := bytes.NewBufferString("----- FOOTER -----\n")
+
+	// 全ての入力を繋げる
+	reader := io.MultiReader(header, content, footer)
+	io.Copy(os.Stdout, reader)
+}
